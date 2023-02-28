6ca5eeb52584c683c0be87ae9cf23314c13313b1
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-02-28 22:03:23 +0000

    ci: add cleaning tool

--- .github/cleaning.py
+++ .github/cleaning.py
@@ -1,15 +1,22 @@
 import nbformat as nbf
 import glob
 
-solutions = glob.glob('../solutions/*/*.ipynb', recursive=True)
+solutions = glob.glob('solutions/*/*.ipynb', recursive=True)
 for solution in solutions:
     ntbk = nbf.read(solution, nbf.NO_CONVERT)
     new_ntbk = ntbk
     for cell in new_ntbk.cells:
+        if cell.cell_type == "markdown":
+            continue
         if cell.hasattr("outputs"):
             cell.outputs = []
         if not cell.metadata.hasattr("tags"):
             continue
-        cell.source = "" if "hide" in cell.metadata.pop("tags") else cell.source
+        
+        tags = cell.metadata.pop("tags")
+        if "hide" in tags:
+            cell.source = []
+        elif "todo" in tags:
+            cell.source = ["## YOUR CODE GOES HERE"]
 
     nbf.write(new_ntbk, solution[10:], version=nbf.NO_CONVERT)
