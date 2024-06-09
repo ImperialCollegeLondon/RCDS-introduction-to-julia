6ea6655a1d136de3c8994b46a1b6f3af820f9d4d
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2024-06-09 14:01:02 +0300

    ci: install Pluto as user

--- .devcontainer/pluto-feature/install.sh
+++ .devcontainer/pluto-feature/install.sh
@@ -2,11 +2,46 @@
 
 set -e
 
-echo "Installing Pluto.jl and running it on port 1234..."
+# Default to using the vscode user
+USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
 
-export JULIA_DEPOT_PATH="/home/vscode/.julia"
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
 
-# Install Pluto.jl
-/home/vscode/.juliaup/bin/julia -e 'import Pkg; Pkg.add("Pluto");'
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u "${USERNAME}" >/dev/null 2>&1; then
+    USERNAME=root
+fi
 
-#Pkg.activate("/home/vscode/.julia/environments/v1.10"); Pkg.precompile();
+echo "Running as user: ${USERNAME}"
+
+# Switch to the correct user to install Pluto
+sudo -u "${USERNAME}" bash << EOF
+    set -e
+    
+    
+    
+    # Activate and install Pluto in the user-specific environment
+    /home/${USERNAME}/.juliaup/bin/julia -e '
+        import Pkg;
+        Pkg.add("Pluto");
+        println("Pluto and its dependencies have been installed and precompiled in the user-specific environment.");
+    '
+    
+    echo "Installation and precompilation completed."
+
+EOF
---
+++ start-pluto.jl
@@ -0,0 +1,4 @@
+#!/usr/bin/env julia
+
+import Pluto
+Pluto.run(require_secret_for_open_links=false, require_secret_for_access=false)
