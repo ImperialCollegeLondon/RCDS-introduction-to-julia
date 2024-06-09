83be5f06151b894a3bd43ea8991d80897dec3c47
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2024-06-09 13:20:01 +0300

    ci: use DEPOT PATH

--- .devcontainer/pluto-feature/install.sh
+++ .devcontainer/pluto-feature/install.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-echo "Installing Pluto.jl and running it on port $PLUTO_PORT..."
+echo "Installing Pluto.jl and running it on port 1234..."
+
+export JULIA_DEPOT_PATH="/home/vscode/.julia"
 
 # Install Pluto.jl
-/home/vscode/.juliaup/bin/julia -e 'import Pkg; Pkg.activate(); Pkg.add("Pluto")'
+/home/vscode/.juliaup/bin/julia -e 'import Pkg; Pkg.add("Pluto");'
+
+#Pkg.activate("/home/vscode/.julia/environments/v1.10"); Pkg.precompile();
