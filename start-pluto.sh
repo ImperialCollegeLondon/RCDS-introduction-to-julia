ca55ad7710a8754bbce596003af2a2de312926ba
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2026-03-15 19:33:43 +0000

    build: include manifest toml

--- start-pluto.sh
+++ start-pluto.sh
@@ -2,5 +2,4 @@
 
 set -eu
 
-[ -f Manifest.toml ] || cp /usr/local/julia-manifest.toml Manifest.toml
 julia --project=. -e 'using Pluto; Pluto.run(require_secret_for_open_links=false, require_secret_for_access=false)'
