c32e8d9cbbe62f59e6b61ba210161199ad7d19ae
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2026-03-15 19:28:19 +0000

    build: improve build

--- start-pluto.sh
+++ start-pluto.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/env sh
+
+set -eu
+
+[ -f Manifest.toml ] || cp /usr/local/julia-manifest.toml Manifest.toml
 julia --project=. -e 'using Pluto; Pluto.run(require_secret_for_open_links=false, require_secret_for_access=false)'
