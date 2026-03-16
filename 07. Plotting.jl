344715105f1322229cf71f9668053bc572b4d3d6
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2026-03-16 11:26:57 +0000

    feat: trim plotting code

--- 07. Plotting.jl
+++ 07. Plotting.jl
@@ -25,7 +25,6 @@ In the name of scientific inquiry, let's use this notebook to examine the relati
 # ╔═╡ f064dc5e-e258-4747-8e95-bf9435ae0c50
 global_temp = [14.4, 14.5, 14.8, 15.2, 15.5, 15.8]
 
-
 # ╔═╡ 2ce938fe-1554-405a-b0fc-36d699faa6b1
 num_pirates = [45000, 20000, 15000, 5000, 400, 17]
 
@@ -34,26 +33,23 @@ md"""
 In CairoMakie, we create a `Figure` and add an `Axis` to it. We can then plot onto that axis using functions like `lines!` and `scatter!`.
 """
 
-# ╔═╡ cc9f2dc1-3d92-4697-8723-5544febbcd6a
-
-# ╔═╡ 18a6cf61-8df1-44c2-a05b-a044e8b183a5
-md"""
-We can use functions like `lines` and `scatter` to generate plots, and combine them by plotting onto the same `Axis`.
-"""
-
 # ╔═╡ 546c1f6f-b95c-4b6c-b18f-9e043fb0b7b8
 ## YOUR CODE GOES HERE
-# ╔═╡ 9a0ba5d9-2d79-47c1-80c4-a7aab24795a8
+# ╔═╡ b1111111-1111-1111-1111-111111111111
+f
+
+# ╔═╡ 18a6cf61-8df1-44c2-a05b-a044e8b183a5
 md"""
 The `!` at the end of the `scatter!` function name makes `scatter!` a mutating function, indicating that the scattered points will be added onto the pre-existing axis.
 
 In contrast, calling `scatter` (without `!`) would create a new figure instead of adding to the existing one.
-
-Next, let's set axis labels and a title by passing keyword arguments to `Axis`.
 """
 
-# ╔═╡ e7c452d8-6169-403e-bf7a-1a5ef880f932
-## YOUR CODE GOES HERE
+# ╔═╡ 9a0ba5d9-2d79-47c1-80c4-a7aab24795a8
+md"""
+We can set axis labels and a title by passing keyword arguments to `Axis`, and add a legend with `axislegend`.
+"""
+
 # ╔═╡ f81f7f23-947c-4d79-9f3a-816e35da4133
 md"""
 !!! warning
@@ -62,11 +58,9 @@ md"""
 
 # ╔═╡ a5c8a475-2b64-4afe-93e2-32e621345ad1
 md"""
-This still doesn't look quite right. The number of pirates has decreased since 1860, so reading the plot from left to right is like looking backwards in time rather than forwards. Let's flip the x axis to better see how pirate populations have caused global temperatures to change over time!
+The number of pirates has decreased since 1860, so reading the plot from left to right is like looking backwards in time. We can flip the x axis with `xreversed=true`.
 """
 
-# ╔═╡ c245eab8-feb9-41b7-857d-3d3d14abe991
-## YOUR CODE GOES HERE
 # ╔═╡ 298da6be-db34-4517-86f1-694970074a5b
 md"""
 And there we have it!
@@ -119,15 +113,17 @@ md"""
 2. Execute the following code:
 """
 
+# ╔═╡ 1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d
+z = -10:10
+
 # ╔═╡ 90f6b5ed-bff9-4e5b-a433-ce06666effd6
-begin
-	z = -10:10
-	fig5 = Figure()
-	ax5a = Axis(fig5[1,1]); lines!(ax5a, z, z)
-	ax5b = Axis(fig5[1,2]); lines!(ax5b, z, z.^2)
-	ax5c = Axis(fig5[2,1]); lines!(ax5c, z, z.^3)
-	ax5d = Axis(fig5[2,2]); lines!(ax5d, z, z.^4)
-	fig5
+let
+	fig = Figure()
+	ax1 = Axis(fig[1,1]); lines!(ax1, z, z)
+	ax2 = Axis(fig[1,2]); lines!(ax2, z, z.^2)
+	ax3 = Axis(fig[2,1]); lines!(ax3, z, z.^3)
+	ax4 = Axis(fig[2,2]); lines!(ax4, z, z.^4)
+	fig
 end
 
 # ╔═╡ f93ee3be-57f3-4d66-9974-1e857e847989
@@ -146,14 +142,12 @@ The Makie documentation on layouts is available [here](https://docs.makie.org/st
 # ╠═f064dc5e-e258-4747-8e95-bf9435ae0c50
 # ╠═2ce938fe-1554-405a-b0fc-36d699faa6b1
 # ╟─e286b95f-e414-4b4f-989d-c9235eb12a7b
-# ╠═cc9f2dc1-3d92-4697-8723-5544febbcd6a
-# ╟─18a6cf61-8df1-44c2-a05b-a044e8b183a5
 # ╠═546c1f6f-b95c-4b6c-b18f-9e043fb0b7b8
+# ╠═b1111111-1111-1111-1111-111111111111
+# ╟─18a6cf61-8df1-44c2-a05b-a044e8b183a5
 # ╟─9a0ba5d9-2d79-47c1-80c4-a7aab24795a8
-# ╠═e7c452d8-6169-403e-bf7a-1a5ef880f932
 # ╟─f81f7f23-947c-4d79-9f3a-816e35da4133
 # ╟─a5c8a475-2b64-4afe-93e2-32e621345ad1
-# ╠═c245eab8-feb9-41b7-857d-3d3d14abe991
 # ╟─298da6be-db34-4517-86f1-694970074a5b
 # ╠═2edd3cfc-9013-4b19-83f2-9b98cbacedcb
 # ╟─627c8d9e-0f07-4c9a-b2bd-5e14426b3fba
@@ -165,6 +159,7 @@ The Makie documentation on layouts is available [here](https://docs.makie.org/st
 # ╠═a7364ac8-20d5-4ace-8b50-8e4c0881ce2a
 # ╟─6255bd68-0b17-4cb4-a4d3-d2614674e8c4
 # ╟─b7379e80-61cc-41dd-b812-e4051f8e1b4a
+# ╠═1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d
 # ╠═90f6b5ed-bff9-4e5b-a433-ce06666effd6
 # ╟─f93ee3be-57f3-4d66-9974-1e857e847989
 # ╠═bc776cd3-a9af-423f-8c21-7e1fc1e9a8cd
--- solutions/07. Plotting.jl
+++ solutions/07. Plotting.jl
@@ -27,7 +27,6 @@ In the name of scientific inquiry, let's use this notebook to examine the relati
 # @keepcode
 global_temp = [14.4, 14.5, 14.8, 15.2, 15.5, 15.8]
 
-
 # ╔═╡ 2ce938fe-1554-405a-b0fc-36d699faa6b1
 # @keepcode
 num_pirates = [45000, 20000, 15000, 5000, 400, 17]
@@ -37,45 +36,35 @@ md"""
 In CairoMakie, we create a `Figure` and add an `Axis` to it. We can then plot onto that axis using functions like `lines!` and `scatter!`.
 """
 
-# ╔═╡ cc9f2dc1-3d92-4697-8723-5544febbcd6a
-
-
-# ╔═╡ 18a6cf61-8df1-44c2-a05b-a044e8b183a5
-md"""
-We can use functions like `lines` and `scatter` to generate plots, and combine them by plotting onto the same `Axis`.
-"""
-
 # ╔═╡ 546c1f6f-b95c-4b6c-b18f-9e043fb0b7b8
 # @solution
 begin
-	fig1 = Figure()
-	ax1 = Axis(fig1[1,1])
+	f = Figure()
+	ax1 = Axis(f[1,1],
+		xlabel="Number of Pirates [Approximate]",
+		ylabel="Global Temperature (C)",
+		title="Influence of pirate population on global warming",
+		xreversed=true)
 	lines!(ax1, num_pirates, global_temp, label="line")
 	scatter!(ax1, num_pirates, global_temp, label="points")
-	fig1
+	axislegend(ax1, position=:lt)
 end
 
-# ╔═╡ 9a0ba5d9-2d79-47c1-80c4-a7aab24795a8
+# ╔═╡ b1111111-1111-1111-1111-111111111111
+# @keepcode
+f
+
+# ╔═╡ 18a6cf61-8df1-44c2-a05b-a044e8b183a5
 md"""
 The `!` at the end of the `scatter!` function name makes `scatter!` a mutating function, indicating that the scattered points will be added onto the pre-existing axis.
 
 In contrast, calling `scatter` (without `!`) would create a new figure instead of adding to the existing one.
-
-Next, let's set axis labels and a title by passing keyword arguments to `Axis`.
 """
 
-# ╔═╡ e7c452d8-6169-403e-bf7a-1a5ef880f932
-# @solution
-begin
-	fig2 = Figure()
-	ax2 = Axis(fig2[1,1],
-		xlabel="Number of Pirates [Approximate]",
-		ylabel="Global Temperature (C)",
-		title="Influence of pirate population on global warming")
-	lines!(ax2, num_pirates, global_temp, label="line")
-	scatter!(ax2, num_pirates, global_temp, label="points")
-	fig2
-end
+# ╔═╡ 9a0ba5d9-2d79-47c1-80c4-a7aab24795a8
+md"""
+We can set axis labels and a title by passing keyword arguments to `Axis`, and add a legend with `axislegend`.
+"""
 
 # ╔═╡ f81f7f23-947c-4d79-9f3a-816e35da4133
 md"""
@@ -85,23 +74,8 @@ md"""
 
 # ╔═╡ a5c8a475-2b64-4afe-93e2-32e621345ad1
 md"""
-This still doesn't look quite right. The number of pirates has decreased since 1860, so reading the plot from left to right is like looking backwards in time rather than forwards. Let's flip the x axis to better see how pirate populations have caused global temperatures to change over time!
+The number of pirates has decreased since 1860, so reading the plot from left to right is like looking backwards in time. We can flip the x axis with `xreversed=true`.
 """
-
-# ╔═╡ c245eab8-feb9-41b7-857d-3d3d14abe991
-# @keepcode
-# @solution
-begin
-	fig3 = Figure()
-	ax3 = Axis(fig3[1,1],
-		xlabel="Number of Pirates [Approximate]",
-		ylabel="Global Temperature (C)",
-		title="Influence of pirate population on global warming",
-		xreversed=true)
-	lines!(ax3, num_pirates, global_temp, label="line")
-	scatter!(ax3, num_pirates, global_temp, label="points")
-	fig3
-end
 
 # ╔═╡ 298da6be-db34-4517-86f1-694970074a5b
 md"""
@@ -133,12 +107,11 @@ Let's plot a sine wave!
 x = 0:0.01:2π # an array with values 0 to 2π , at a step of 0.1
 
 # ╔═╡ 99ed55df-8d00-4954-890e-4dd04bad3bd4
-# @solution
-begin
-	fig_sin = Figure()
-	ax_sin = Axis(fig_sin[1,1], title="Sine Wave", limits=(nothing, (-1.5, 1.5)))
-	lines!(ax_sin, x, sin.(x))
-	fig_sin
+let
+	fig = Figure()
+	ax1 = Axis(fig[1,1], title="Sine Wave", limits=(nothing, (-1.5, 1.5)))
+	lines!(ax1, x, sin.(x))
+	fig
 end
 
 # ╔═╡ 4d73f3e6-579c-4555-9207-a8310efeaf54
@@ -154,13 +127,12 @@ plot y vs. x for $y = x^2$.  If you run into an issue, look at the hint below.
 
 # ╔═╡ a7364ac8-20d5-4ace-8b50-8e4c0881ce2a
 # @solution
-# @solution
 let
 	x = -10:10
 	y = x .^ 2
 	fig = Figure()
-	ax = Axis(fig[1,1])
-	lines!(ax, x, y)
+	ax1 = Axis(fig[1,1])
+	lines!(ax1, x, y)
 	fig
 end
 
@@ -175,16 +147,19 @@ md"""
 2. Execute the following code:
 """
 
+# ╔═╡ 1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d
+# @keepcode
+z = -10:10
+
 # ╔═╡ 90f6b5ed-bff9-4e5b-a433-ce06666effd6
 # @keepcode
-begin
-	z = -10:10
-	fig5 = Figure()
-	ax5a = Axis(fig5[1,1]); lines!(ax5a, z, z)
-	ax5b = Axis(fig5[1,2]); lines!(ax5b, z, z.^2)
-	ax5c = Axis(fig5[2,1]); lines!(ax5c, z, z.^3)
-	ax5d = Axis(fig5[2,2]); lines!(ax5d, z, z.^4)
-	fig5
+let
+	fig = Figure()
+	ax1 = Axis(fig[1,1]); lines!(ax1, z, z)
+	ax2 = Axis(fig[1,2]); lines!(ax2, z, z.^2)
+	ax3 = Axis(fig[2,1]); lines!(ax3, z, z.^3)
+	ax4 = Axis(fig[2,2]); lines!(ax4, z, z.^4)
+	fig
 end
 
 # ╔═╡ f93ee3be-57f3-4d66-9974-1e857e847989
@@ -196,14 +171,13 @@ The Makie documentation on layouts is available [here](https://docs.makie.org/st
 
 # ╔═╡ bc776cd3-a9af-423f-8c21-7e1fc1e9a8cd
 # @solution
-# @solution
-begin
-	fig6 = Figure()
-	ax6a = Axis(fig6[1,1]); lines!(ax6a, z, z)
-	ax6b = Axis(fig6[2,1]); lines!(ax6b, z, z.^2)
-	ax6c = Axis(fig6[3,1]); lines!(ax6c, z, z.^3)
-	ax6d = Axis(fig6[4,1]); lines!(ax6d, z, z.^4)
-	fig6
+let
+	fig = Figure()
+	ax1 = Axis(fig[1,1]); lines!(ax1, z, z)
+	ax2 = Axis(fig[2,1]); lines!(ax2, z, z.^2)
+	ax3 = Axis(fig[3,1]); lines!(ax3, z, z.^3)
+	ax4 = Axis(fig[4,1]); lines!(ax4, z, z.^4)
+	fig
 end
 
 # ╔═╡ Cell order:
@@ -213,14 +187,12 @@ end
 # ╠═f064dc5e-e258-4747-8e95-bf9435ae0c50
 # ╠═2ce938fe-1554-405a-b0fc-36d699faa6b1
 # ╟─e286b95f-e414-4b4f-989d-c9235eb12a7b
-# ╠═cc9f2dc1-3d92-4697-8723-5544febbcd6a
-# ╟─18a6cf61-8df1-44c2-a05b-a044e8b183a5
 # ╠═546c1f6f-b95c-4b6c-b18f-9e043fb0b7b8
+# ╠═b1111111-1111-1111-1111-111111111111
+# ╟─18a6cf61-8df1-44c2-a05b-a044e8b183a5
 # ╟─9a0ba5d9-2d79-47c1-80c4-a7aab24795a8
-# ╠═e7c452d8-6169-403e-bf7a-1a5ef880f932
 # ╟─f81f7f23-947c-4d79-9f3a-816e35da4133
 # ╟─a5c8a475-2b64-4afe-93e2-32e621345ad1
-# ╠═c245eab8-feb9-41b7-857d-3d3d14abe991
 # ╟─298da6be-db34-4517-86f1-694970074a5b
 # ╠═2edd3cfc-9013-4b19-83f2-9b98cbacedcb
 # ╟─627c8d9e-0f07-4c9a-b2bd-5e14426b3fba
@@ -232,6 +204,7 @@ end
 # ╠═a7364ac8-20d5-4ace-8b50-8e4c0881ce2a
 # ╟─6255bd68-0b17-4cb4-a4d3-d2614674e8c4
 # ╟─b7379e80-61cc-41dd-b812-e4051f8e1b4a
+# ╠═1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d
 # ╠═90f6b5ed-bff9-4e5b-a433-ce06666effd6
 # ╟─f93ee3be-57f3-4d66-9974-1e857e847989
 # ╠═bc776cd3-a9af-423f-8c21-7e1fc1e9a8cd
